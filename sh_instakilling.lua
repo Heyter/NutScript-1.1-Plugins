97fb7e770d1fcba08872578004148d74bf7ac283
SuperMicronde <supermicronde3@gmail.com>
2017-08-24 11:11:12 +0200

    Add files via upload

---
+++ sh_instakilling.lua
@@ -0,0 +1,30 @@
+PLUGIN.name = "Instakilling"
+PLUGIN.author = "SuperMicronde"
+PLUGIN.desc = "Instakilling when head or neck is shooted"
+
+nut.config.add("instakilling", true, "Instakilling activated ?", nil, {
+	category = "server"
+})
+
+hook.Add("EntityTakeDamage", "PlayerTakeDamageAtHead", function( target, dmg )
+	
+	if nut.config.get("instakilling", true) then
+	
+		local victimHealth = target:Health()
+		
+		if IsValid(target) && dmg:GetDamage() <= victimHealth && dmg:IsDamageType( DMG_BULLET ) then
+			
+			local shootPos = dmg:GetDamagePosition()
+			local bonePos = target:GetBonePosition(target:LookupBone( "ValveBiped.Bip01_Head1" ))
+			
+			if shootPos:Distance(bonePos) < 9 then
+				
+				dmg:SetDamage( victimHealth + math.random( 1, 23 ))
+			
+			end
+			
+		end
+		
+	end	
+
+end)
